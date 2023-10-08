a099263f4d78de6c60a223ce30a62bb5fe8d1230
Ali Dowair <adowair@umich.edu>
2023-10-08 23:47:03 +0300

    Set() to take time parameter
    In order to simply inject times for testing. It was also
    possible to pass in a timer, or to monkeypatch the test code.
    However, this approach is simple and clear (although the
    public interface for Set() is now not perfectly clean.

--- kv/ops.go
+++ kv/ops.go
@@ -9,7 +9,7 @@ func Get(key string) (string, error) {
 	return "", errors.ErrUnsupported
 }
 
-func Set(key, val string) error {
+func Set(key, val string, now time.Time) error {
 	return errors.ErrUnsupported
 }
 
