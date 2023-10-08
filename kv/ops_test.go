126f329b3de66aa9ce9d1e39852292395f25270f
Ali Dowair <adowair@umich.edu>
2023-10-08 23:48:36 +0300

    Implement operations library functions
    These functions are invoked by the CLI (cmd) package, and perform
    any data mutation and validation before using low-level utils
    to read and write data.

--- kv/ops_test.go
+++ kv/ops_test.go
@@ -28,45 +28,165 @@ type step struct {
 // expReturns encapsulates possible returns for any operation. All fields
 // are optional depending on the operation being tested, except for `expError`.
 type expReturns struct {
-	expError    error
-	expValue    string
-	expFirstSet time.Time
-	expLastSet  time.Time
+	err      error
+	value    string
+	firstSet time.Time
+	lastSet  time.Time
 }
+
+var (
+	time0 = time.Unix(0, 0)
+	time1 = time.Unix(1, 0)
+)
 
 func testGet(key string) func(*testing.T, *expReturns) {
 	return func(t *testing.T, r *expReturns) {
-		ret, err := kv.Get(key)
-		assert.ErrorIs(t, err, r.expError)
-		assert.Equal(t, ret, r.expValue)
+		value, err := kv.Get(key)
+		assert.ErrorIs(t, err, r.err)
+		assert.Equal(t, r.value, value)
 	}
 }
 
-func testSet(key string, val string) func(*testing.T, *expReturns) {
+func testSet(key, val string, now time.Time) func(*testing.T, *expReturns) {
 	return func(t *testing.T, r *expReturns) {
-		err := kv.Set(key, val)
-		assert.ErrorIs(t, err, r.expError)
+		err := kv.Set(key, val, now)
+		assert.ErrorIs(t, err, r.err)
 	}
 }
 
 func testTimestamp(key string) func(*testing.T, *expReturns) {
 	return func(t *testing.T, r *expReturns) {
-		first, last, err := kv.Timestamps(key)
-		assert.ErrorIs(t, err, r.expError)
-		assert.Equal(t, first, r.expFirstSet)
-		assert.Equal(t, last, r.expLastSet)
+		firstSet, lastSet, err := kv.Timestamps(key)
+		assert.ErrorIs(t, err, r.err)
+		assert.Equal(t, r.firstSet, firstSet, "firstSet")
+		assert.Equal(t, r.lastSet, lastSet, "lastSet")
 	}
 }
 
 func testDelete(key string) func(*testing.T, *expReturns) {
 	return func(t *testing.T, r *expReturns) {
 		err := kv.Delete(key)
-		assert.ErrorIs(t, err, r.expError)
+		assert.ErrorIs(t, err, r.err)
 	}
 }
 
 func TestKVOps(t *testing.T) {
-	for _, tc := range []testcase{} {
+	for _, tc := range []testcase{
+		{
+			name: "TestGetNotExist",
+			steps: []step{
+				{
+					run:        testGet("a"),
+					expReturns: expReturns{err: kv.ErrNotExist},
+				},
+			},
+		},
+		{
+			name: "TestDoubleGetNotExist",
+			steps: []step{
+				{
+					run:        testGet("a"),
+					expReturns: expReturns{err: kv.ErrNotExist},
+				},
+				{
+					run:        testGet("b"),
+					expReturns: expReturns{err: kv.ErrNotExist},
+				},
+			},
+		},
+		{
+			name: "TestSetGet",
+			steps: []step{
+				{
+					run:        testSet("a", "12", time0),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testGet("a"),
+					expReturns: expReturns{err: nil, value: "12"},
+				},
+				{
+					run:        testSet("b", "13", time0),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testGet("b"),
+					expReturns: expReturns{err: nil, value: "13"},
+				},
+				{
+					run:        testSet("a", "14", time1),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testGet("a"),
+					expReturns: expReturns{err: nil, value: "14"},
+				},
+			},
+		},
+		{
+			name: "TestSetTS",
+			steps: []step{
+				{
+					run:        testSet("a", "12", time0),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testTimestamp("a"),
+					expReturns: expReturns{err: nil, firstSet: time0, lastSet: time0},
+				},
+				{
+					run:        testSet("b", "13", time1),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testTimestamp("b"),
+					expReturns: expReturns{err: nil, firstSet: time1, lastSet: time1},
+				},
+				{
+					run:        testSet("a", "13", time1),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testTimestamp("a"),
+					expReturns: expReturns{err: nil, firstSet: time0, lastSet: time1},
+				},
+			},
+		},
+		{
+			name: "TestDeleteTS",
+			steps: []step{
+				{
+					run:        testSet("a", "12", time0),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testDelete("a"),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testTimestamp("a"),
+					expReturns: expReturns{err: kv.ErrNotExist},
+				},
+			},
+		},
+		{
+			name: "TestDeleteGet",
+			steps: []step{
+				{
+					run:        testSet("a", "12", time0),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testDelete("a"),
+					expReturns: expReturns{err: nil},
+				},
+				{
+					run:        testGet("a"),
+					expReturns: expReturns{err: kv.ErrNotExist},
+				},
+			},
+		},
+	} {
 		t.Run(tc.name, func(t *testing.T) {
 			dir := t.TempDir()
 			assert.NoError(t, os.Chdir(dir))
